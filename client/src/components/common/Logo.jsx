570bddf29384d6fc549bc08eb0cfbd18d60b1681
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-10-25 22:03:34 +0530

    ADD: MovieMetropolis

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
 
   return (
     <Typography fontWeight="700" fontSize="1.7rem">
-        Movies<span style={{ color: theme.palette.primary.main }}>Flix</span>
+        Movie<span style={{ color: theme.palette.primary.main }}>Metropolis</span>
     </Typography>
   );
 };
