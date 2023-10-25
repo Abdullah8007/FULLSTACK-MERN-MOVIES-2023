e88c2cb9a12ea6266d77938b80c5033e2f52f7e1
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-10-25 22:02:38 +0530

    ADD: MovieMetropolis

--- client/src/configs/theme.configs.js
+++ client/src/configs/theme.configs.js
@@ -10,7 +10,7 @@ const themeConfigs = {
   custom: ({ mode }) => {
     const customPalette = mode === themeModes.dark ? {
       primary: {
-        main: "#38acec",
+        main: "#16F529",
         contrastText: "#ffffff"
       },
       secondary: {
@@ -23,7 +23,7 @@ const themeConfigs = {
       }
     } : {
       primary: {
-        main: "#38acec"
+        main: "#16F529"
       },
       secondary: {
         main: "#f44336"
