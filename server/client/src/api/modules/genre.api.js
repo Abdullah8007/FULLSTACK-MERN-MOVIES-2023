c7b8cb18d7a80f17867ad60f16bc46662fc0f100
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-10-26 17:52:01 +0530

    ADD: MovieMetropolis

---
+++ server/client/src/api/modules/genre.api.js
@@ -0,0 +1,21 @@
+import publicClient from "../client/private.client.js";
+
+const genreEndpoints = {
+    list: ({ mediaType }) => `${mediaType}/genres`
+};
+
+const genreApi = {
+    getList: async ({ mediaType }) => {
+        try {
+            const response = await publicClient.get(genreEndpoints.list({ mediaType }));
+
+            return { response };
+        }
+
+        catch (err) {
+            return { err };
+        }
+    }
+};
+
+export default genreApi;
